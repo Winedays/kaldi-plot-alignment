d5744422756772ab2ede17534a5d9e47dc34b4eb
Winedays <adler011014@gmail.com>
2019-11-13 20:06:02 +0800

    add label, text, time for graph add ctm_opts for plot_word_ali.sh

--- plot_word_ali.sh
+++ plot_word_ali.sh
@@ -5,12 +5,17 @@ task=test
 expdir=./exp/chain_tdnnf/  # exp result directory with special model
 ctmdir=./temp/  # path to save the ctm result
 datadir=./data/  # data directory include wav.scp & text
+savedir=./temp/graph  # path to save the wave graph
+ctm_opts="--frame-shift 0.03"  # options of get_ctm_fast.sh without lmwt.
+                               # if you want to have a grap for chain model , you should set frame-shift 
+							   # base on your model skip-frame setting.
 
 . utils/parse_options.sh || exit 1;
 
 expdir=$expdir/decode_$task/
 ctmdir=$ctmdir/ctm_$task/
 datadir=$datadir/$task/
+savedir=$ctmdir/graph
 
 # get ctm result 
 if [ ! -d $ctmdir ]; then
@@ -18,10 +23,10 @@ if [ ! -d $ctmdir ]; then
 fi
 lmwt=`cat ${expdir}/scoring_kaldi/best_wer | awk -F"/" '{print $5}' | awk -F"_" '{print $2}'` 
 echo "lmwt:" $lmwt
-./steps/get_ctm_fast.sh --lmwt $lmwt --frame-shift 0.03 data/$task/ lang/$task/ $expdir $ctmdir
-# ctm format : utt_id channel_num start_time phone_dur phone_id
+./steps/get_ctm_fast.sh --lmwt $lmwt $ctm_opts data/$task/ lang/$task/ $expdir $ctmdir
+# ctm format : utt_id channel_num start_time phone_dur text
 
 # draw wave form & render phone boundary
 ctmfile=$ctmdir/ctm
-python3 ./local/plot_word_bowndary.py --ctm $ctmfile --data $datadir --save $ctmdir
+python3 ./local/plot_word_bowndary.py --ctm $ctmfile --data $datadir --save $savedir
 
--- plot_word_bowndary.py
+++ plot_word_bowndary.py
@@ -11,8 +11,11 @@ import random
 
 '''
 How to plot a wav file : https://stackoverflow.com/questions/18625085/how-to-plot-a-wav-file/18625294
+matplotlib: how to draw a rectangle on image : https://stackoverflow.com/questions/37435369/matplotlib-how-to-draw-a-rectangle-on-image
 wave packege document : https://docs.python.org/3/library/wave.html
 matplotlib.pyplot.ax document : https://matplotlib.org/3.1.1/api/axes_api.html
+Common xlabel/ylabel for matplotlib subplots : https://stackoverflow.com/questions/16150819/common-xlabel-ylabel-for-matplotlib-subplots
+
 '''
 
 def setArgument() :
@@ -40,24 +43,25 @@ def readWavscp( wavscp ) :
 # read ctm file
 def readCtm( ctm ) :
     ctmDict = {}
-    # ctm format : utt_id channel_num start_time phone_dur phone_id
+    # ctm format : utt_id channel_num start_time phone_dur text
     f = open( ctm , 'r' , encoding='utf-8' )
     rows = csv.reader( f , delimiter=' ' )
     for row in rows :
         utt_id = row[0]
         start_time = float(row[2])
         phone_dur = float(row[3])
-        phone_id = int(row[4])
+        text = int(row[4])
         end_time = round( start_time+phone_dur , 3 )
         if utt_id not in ctmDict :
             ctmDict[ utt_id ] = []
-        ctmDict[ utt_id ].append( { 'phone_id': phone_id, 'start_time': start_time, 'end_time': end_time, 'phone_dur': phone_dur } )
+        ctmDict[ utt_id ].append( { 'text': text, 'start_time': start_time, 'end_time': end_time, 'phone_dur': phone_dur } )
     f.close()
     # print( ctmDict )
     return ctmDict
   
 # draw wave form & render phone boundary  
 def plotWav( utt_id , cmtList , wavFile , saveFile ) :
+    print( "Plot wave of " + wavFile )
     spf = wave.open( wavFile , 'r' )
     
     # If Stereo
@@ -72,10 +76,14 @@ def plotWav( utt_id , cmtList , wavFile , saveFile ) :
     fs = spf.getframerate()  # sampling frequency.
     Time=np.linspace(0, len(signal)/fs, num=len(signal))  # Time Vector spaced linearly with the size
 
+
     # draw wave form
     fig,ax = plt.subplots(1)
-    ax.set_title( 'Signal Wave... '+utt_id )
+    ax.set_title( utt_id + ' Signal Wave' , pad=30 )
     ax.set_ylim(-fs, fs)
+    ax.set_xlabel( "Time(s)" )
+    ax.set_ylabel( "frequency" , labelpad=-5 )
+    ax.text( Time[-1]/2 , fs+1250 , "Text" , ha='center' , va='bottom' , color='k' )  # add label on the top for text 
     ax.plot(Time,signal)
     # render phone boundary
     ax = wavPltAddRectangle( ax , cmtList )
@@ -91,15 +99,20 @@ def plotWav( utt_id , cmtList , wavFile , saveFile ) :
 # render phone boundary : add rectangle to the graph
 def wavPltAddRectangle( ax , cmtList , fs=16000 ) :
     for cmt in cmtList :
-        text = cmt['phone_id']
+        text = cmt['text']
         startTime = cmt['start_time']
         phoneDur = cmt['phone_dur']    
         colour = gemRandomColour()
         # Create a Rectangle patch
-        rect = patches.Rectangle( (startTime,-fs+1) , phoneDur , fs*2-1 , linewidth=1 , edgecolor=colour , facecolor='none' , zorder=99) # (x,y), width, height
+        rect = patches.Rectangle( (startTime,-fs) , phoneDur , fs*2 , linewidth=1 , edgecolor=colour , facecolor='none' , zorder=99) # (x,y), width, height
         # Add the patch to the Axes
         ax.add_patch(rect)
-        ax.text(startTime+phoneDur/2, -fs+20, text, color='k', fontweight='bold')
+        # Add text to Axes
+        ax.text( startTime+phoneDur/2 , fs , text , ha='center' , va='bottom' , color='k' )  # add text
+        ax.text( startTime , -fs-100 , startTime , ha='center' , va='top' , color='k' )  # add start time
+    # add end time of final word
+    end_time = cmtList[-1]['end_time']
+    ax.text( end_time , -fs-100 , end_time , ha='center' , va='top' , color='k' )  
     return ax 
     
 # generate random colors for matplotlib
@@ -129,7 +142,10 @@ if __name__ == "__main__" :
     wavscpFile = os.path.join( dataPath , 'wav.scp' )
     wavDict = readWavscp( wavscpFile )
     ctmDict = readCtm( ctmFile )
-    saveFile = os.path.join( savePath, 'wav.png' )
     
     # drow graph
-    plotWav( 'md011010' , ctmDict['md011010'] , wavDict['md011010'] , saveFile )
+    for wav in wavDict :
+        wavPath = wavDict[ wav ]
+        ctmList = ctmDict[ wav ]
+        saveFile = os.path.join( savePath, wav+'.png' )
+        plotWav( wav , ctmList , wavPath , saveFile )
